333618ad1b53a8eb5f406f03cdb25c434c5f5580
Leonel Torres <torresleonel18@gmail.com>
2021-12-05 23:55:06 -0400

    [ADD] car_maintenance: it contains some elementary functions to manage the car maintenance.

---
+++ car_maintenance/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Car maintenance',
+    'summary': 'Car maintenance services.',
+    'description': """
+        It contains some elementary functions to manage the car maintenance.
+
+        Note: developed for job interview.
+    """,
+    'author': 'Leonel Torres',
+    'website': 'https://github.com/torresleonel',
+    'category': 'Services',
+    'version': '15.0.0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'license': 'LGPL-3'
+}
