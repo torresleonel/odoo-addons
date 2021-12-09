79d78b0c182bf776d47b6580ed74add5cf7fe713
Leonel Torres <torresleonel18@gmail.com>
2021-12-09 03:38:05 -0400

    [IMP] car_maintenance: contain demo data.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Job interview test applications.
+# Odoo Addons for job interview.
+
+This repository contains an application (module) for the registration
+of vehicles for maintenance service.
+
+GitHub Action is used to generate continuous integration (CI).
+Docker Compose is used for deployment.
--- car_maintenance/__manifest__.py
+++ car_maintenance/__manifest__.py
@@ -8,7 +8,7 @@
         Note: developed for job interview.
     """,
     'author': 'Leonel Torres',
-    'website': 'https://github.com/torresleonel',
+    'website': 'https://github.com/torresleonel/odoo-addons',
     'category': 'Services',
     'version': '15.0.0.1',
     'depends': ['base', 'mail'],
@@ -19,9 +19,7 @@
         'report/car_service_templates.xml',
         'report/car_service_reports.xml'
     ],
-    'demo': [
-        # 'demo/demo.xml',
-    ],
+    'demo': ['demo/car_maintenance_demo.xml'],
     'application': True,
     'license': 'LGPL-3'
 }
